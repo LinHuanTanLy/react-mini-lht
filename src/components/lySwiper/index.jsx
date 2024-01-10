32342f66d80d5da946c89fb194266c144c6e509b
lht <ly137544897@outlook.com>
2024-01-10 14:28:57 +0800

    commit

---
+++ src/components/lySwiper/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./style.less"
+
+
+const LySwiper = () => {
+    return (
+
+
+        <div></div>
+    )
+}
+
+export default LySwiper;
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeaderNav from "../../components/headerNav";
+import LySwiper from '../../components/lySwiper'
 const Home = () => {
     return <div>
 
         <HeaderNav></HeaderNav>
-        
+        <LySwiper></LySwiper>
+
     </div>
 }
 
